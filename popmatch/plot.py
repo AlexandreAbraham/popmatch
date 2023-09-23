8de0eeb281f953d88d4f29e458196eb14ecd6705
Alexandre Abraham <abraham.alexandre@gmail.com>
2023-09-23 08:00:59 +0200

    Add plotting code

---
+++ popmatch/plot.py
@@ -0,0 +1,24 @@
+from matplotlib import pyplot as plt
+import pandas as pd
+import itertools
+import seaborn as sns
+
+
+
+def plot_smds(experiment, split_ids, matchings):
+    smds = []
+    for splitid, matching in itertools.product(split_ids, matchings):
+        smd = experiment[splitid][f"{matching}_smds"]
+        smd['split_id'] = splitid
+        smd['matching'] = matching
+        smds.append(smd)
+    smds = pd.concat(smds)
+    sns.catplot(
+        y="feature",       # x variable name
+        x="smd",       # y variable name
+        hue="matching",  # group variable name
+        data=smds,     # dataframe to plot
+        kind="bar",
+        orient='h',
+    )
+    plt.savefig('smds.png')
